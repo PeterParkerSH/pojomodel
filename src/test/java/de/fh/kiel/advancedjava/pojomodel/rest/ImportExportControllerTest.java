a7feae3058d3243b74af1ba16c5257fe0636b267
Vera <vera.brennecke@stud.hs-hannover.de>
2021-05-16 21:48:39 +0200

    add unit test for json import

---
+++ src/test/java/de/fh/kiel/advancedjava/pojomodel/rest/ImportExportControllerTest.java
@@ -0,0 +1,51 @@
+package de.fh.kiel.advancedjava.pojomodel.rest;
+
+import de.fh.kiel.advancedjava.pojomodel.repository.PojoElementRepository;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ImportExportControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    PojoElementRepository pojoElementRepository;
+
+    @BeforeEach
+    void clearNodes(){
+        pojoElementRepository.deleteAll();
+    }
+
+    MockMultipartFile getMockMultipartFileFromResource(String resource) throws URISyntaxException, IOException {
+        URL url = this.getClass().getClassLoader().getResource(resource);
+        File file = new File(url.toURI());
+        FileInputStream fis = new FileInputStream(file);
+        return new MockMultipartFile("json", FilenameUtils.getName(url.getFile()), "application/octet-stream", IOUtils.toByteArray(fis));
+    }
+
+    @Test
+    void jsonImport() throws Exception{
+        MockMultipartFile json = getMockMultipartFileFromResource("sampleJson.json");
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/jsonImport").file(json)).andExpect(status().isOk());;
+        assertNotNull(pojoElementRepository.findAll());
+    }
+}
