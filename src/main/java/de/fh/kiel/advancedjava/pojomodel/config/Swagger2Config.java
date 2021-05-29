6cb5be8ee36b03f7823afd84df2a75d6cdf37b51
Vera <vera.brennecke@stud.hs-hannover.de>
2021-05-29 16:04:01 +0200

    swagger ui working

--- src/main/java/de/fh/kiel/advancedjava/pojomodel/config/Swagger2Config.java
+++ src/main/java/de/fh/kiel/advancedjava/pojomodel/config/Swagger2Config.java
@@ -16,10 +16,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
                 .apis(RequestHandlerSelectors
-                        .basePackage("net.guides.springboot2.springboot2swagger2.controller"))
-                .paths(PathSelectors.regex("/.*"))
+                        .any())
+                .paths(PathSelectors.any())
                 .build().apiInfo(apiEndPointsInfo());
     }
 
