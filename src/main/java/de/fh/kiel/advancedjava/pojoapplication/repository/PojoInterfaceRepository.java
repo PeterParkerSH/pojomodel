5445482a7ce7134418de01d90dccb7266d936ba2
Vera <vera.brennecke@stud.hs-hannover.de>
2021-06-19 16:20:07 +0200

    more javadoc

--- src/main/java/de/fh/kiel/advancedjava/pojoapplication/repository/PojoInterfaceRepository.java
+++ src/main/java/de/fh/kiel/advancedjava/pojoapplication/repository/PojoInterfaceRepository.java
@@ -9,12 +9,19 @@ import org.springframework.data.repository.query.Param;
  * Neo4j repository for Objects of type {@link PojoInterface}
  */
 public interface PojoInterfaceRepository extends Neo4jRepository<PojoInterface, Long> {
-    @Query("MATCH (i:Interface) WHERE i.name = $name AND i.packageName = $packageName RETURN i")
-    PojoInterface getPojoInterfaceByNameAndPackageName(@Param("name") String name, @Param("packageName") String packageName);
 
+    /**
+     * Change type of given {@link de.fh.kiel.advancedjava.pojoapplication.pojomodel.PojoReference} to {@link PojoInterface}
+     * @param id id of PojoReference
+     * @return PojoInterface
+     */
     @Query("MATCH (n:Element) WHERE ID(n) = $id REMOVE n:Reference SET n:Interface RETURN n")
     PojoInterface changeReferenceToInterfaceById(@Param("id") Long id);
 
+    /**
+     * Remove the label and fields from a given PojoInterface
+     * @param id id of PojoInterface
+     */
     @Query("MATCH (n:Interface) WHERE ID(n) = $id REMOVE n:Interface")
     void removeInterfaceLabelAndFieldsById(@Param("id") Long id);
 
